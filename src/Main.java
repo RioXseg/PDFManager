d19bed9629a196b1f6b0057474d3e983b0a4b96b
Sergio <ser.alvarez1998@gmail.com>
2018-08-20 23:16:08 -0300

    Comentarios

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,18 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 
 import java.io.IOException;
 
-public class main {
+public class Main {
 
-    public static void main (String args[]) throws IOException {
+    public static void main (String args[]){
 
         //Creamos manager
         PDFManager manager = new PDFManager(System.getProperty("user.dir") + "/In.pdf",System.getProperty("user.dir") + "/test.pdf");
 
+        //Clonamos paginas pedidas
         manager.clone(11,16);
         manager.addBlancPageToAll();
 
+        //Guardamos y cerramos
         manager.save();
         manager.close();
     }
